feb48913c41d03b1acc3710ce9da66f4dde931a7
Neel Krishnatray <neelkrishnatray@Anneta-Mac-Mini.fritz.box>
2026-04-23 19:08:14 +0200

    added new weight calculation and normalized the score

--- confidence_score.py
+++ confidence_score.py
@@ -17,14 +17,33 @@ def sigmoid_activ():
     "Cell culture / in vitro studies": 2/6,
     "In silico / computational predictions": 1/6
     }
-    result = {"positive":1,"negative":-1,"neutral":-0.5,"unclear":0.05}
+    effect_type = {
+    "lifespan": 1.0, 
+    "healthspan": 5/6, 
+    "functional" : 4/6,
+    "biomarker" : 3/6, 
+    "mechanistic": 2/6, 
+    "computational": 1/6, 
+    "unclear" : 0
+    }
+    result = {"positive":1.0,"negative":-1.0,"neutral":-0.5,"unclear":0.05}
     sum = 0
+    n = 0
     for paper in data['papers']: 
-        w = weights.get(paper['study_type'])
+        w = weights.get(paper['study_type'])  
         r = result.get(paper['study_result'])
-    sum += (w*r) 
+        e = effect_type.get(paper['effect_type'])
+        #calculation the score
+        if (w== None) | (r == None) | (e==None):
+            continue
+        sum += (w*r*e)
+        n += w
+    #normalize score 
+    sum = (sum)/(n**0.5)
     score = 1/(1+np.exp(-1*sum))
     data['confidence_score'] = score
     with open('data/processed/classified_papers.json','w', encoding = 'utf-8') as file: 
         json.dump(data,file,indent=4)
-    print("sucessfull")
+    print(score)
+
+sigmoid_activ()
